0f595f18f6755a23995567c0613b7713955326fb
Julian Buitrago <reus1410hotmail.com>
2025-03-05 17:49:31 -0500

    Funcion volumen cubo y area superficie cubo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -187,7 +187,7 @@ class Geometria:
         Returns:
             float: Volumen del cubo
         """
-        pass
+        return lado**3
     
     def area_superficie_cubo(self, lado):
         """
@@ -199,7 +199,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cubo
         """
-        pass
+        return 6 * lado**2
     
     def volumen_esfera(self, radio):
         """
