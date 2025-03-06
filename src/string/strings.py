a2b481342fb92f78183a5ffd6cd34b6fbb1eae56
Julian Buitrago <reus1410hotmail.com>
2025-03-06 06:18:13 -0500

    Funcion contar vocales y funcino contar consonantes

--- src/string/strings.py
+++ src/string/strings.py
@@ -43,7 +43,12 @@ class Strings:
         Returns:
             int: Número de vocales en la cadena
         """
-        pass
+        vocales = "aeiouAEIOU"
+        contador = 0
+        for caracter in texto:
+            if caracter in vocales:
+                contador += 1
+        return contador
     
     def contar_consonantes(self, texto):
         """
@@ -55,7 +60,9 @@ class Strings:
         Returns:
             int: Número de consonantes en la cadena
         """
-        pass
+        vocales = "aeiouáéíóúüAEIOU"
+        return sum(1 for caracter in texto.lower() if caracter.isalpha() and caracter not in vocales)
+
     
     def es_anagrama(self, texto1, texto2):
         """
